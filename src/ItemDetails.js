149c8f3da654ff37a8c6d2fbf065eded2e0dd42a
zeinabalayann75 <zeinabalayann75@gmail.com>
2021-12-22 14:36:27 +0200

    first commit

--- src/ItemDetails.js
+++ src/ItemDetails.js
@@ -1,28 +1,13 @@
 import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
-import { makeStyles } from '@material-ui/core/styles';
 import {Modal,Button, Item} from "semantic-ui-react"
 import { useState } from 'react';
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-  leftIcon: {
-    marginRight: theme.spacing(1),
-  },
-  rightIcon: {
-    marginLeft: theme.spacing(0),
-  },
-  iconSmall: {
-    fontSize: 20,
-  },
-}))
+
 const ItemDetails = () => {
   const { id } = useParams();
   const { data: item, error, isPending } = useFetch('http://localhost:8001/items/' + id);
   const { data: item2} = useFetch('http://localhost:8001/items2/' + id);
   const history = useHistory();
-  const classes = useStyles();
   const [open, setOpen] = useState(false);
   const [itemName, setItemName] = useState('');
   const [age, setAge] = useState('');
@@ -30,17 +15,26 @@ const ItemDetails = () => {
   const [itemColor, setItemcolor] = useState('');
   const [about, setAbout] = useState('');
   
-const handleSubmit=(item)=>{
+const handleSubmit=()=>{
  
   const newItems={age:age,itemName:itemName,phone:phone,itemColor:itemColor,about:about}
-
-  fetch('http://localhost:8001/items/'+item.id, {
+if(item !==null){fetch('http://localhost:8001/items/'+item.id, {
       method: 'PUT',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newItems)
     }).then(response => {response.json();window.location.reload()})
 
 }
+else if(item2 !==null)
+{
+  fetch('http://localhost:8001/items2/'+item2.id, {
+      method: 'PUT',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newItems)
+    }).then(response => {response.json();window.location.reload()})
+}
+}
+  
   const handleClick = () => {
     if(item !==null){fetch('http://localhost:8001/items/' + item.id, {
       method: 'DELETE'
@@ -157,7 +151,7 @@ if(item!==null){
           content="Update"
           labelPosition='right'
           icon='checkmark'
-          onClick={() => handleSubmit(item)}
+          onClick={ handleSubmit}
           positive
           className="edit-button"
         />
